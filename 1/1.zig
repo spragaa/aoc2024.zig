d2aba80b72dc0ae64d507a146fec351e029934e0
Ryuji Kimura <108902150+spragaa@users.noreply.github.com>
2025-01-25 16:24:38 +0200

    solve task 1 part 2

--- 1/1.zig
+++ 1/1.zig
@@ -6,6 +6,45 @@ fn lessThan(context: void, a: i32, b: i32) bool {
     return a < b;
 }
 
+fn partOne(left_column: std.ArrayList(i32), right_column: std.ArrayList(i32)) !i32 {
+    std.mem.sort(i32, left_column.items, {}, lessThan);
+    std.mem.sort(i32, right_column.items, {}, lessThan);
+
+    var result: i32 = 0;
+    for (left_column.items, right_column.items) |item1, item2| {
+        result += @intCast(@abs(item1 - item2));
+    }
+    
+    return result;
+}
+
+fn partTwo(left_column: std.ArrayList(i32), right_column: std.ArrayList(i32)) !i32 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var result: i32 = 0;
+    var freq = std.AutoHashMap(i32, i32).init(allocator);
+    defer freq.deinit();
+
+    for (right_column.items) |item| {
+        const entry = try freq.getOrPut(item);
+        if (!entry.found_existing) {
+            entry.value_ptr.* = 0;
+        }
+        entry.value_ptr.* += 1;
+    }
+
+    for (left_column.items) |item| {
+        if (freq.contains(item)) {
+            const count = freq.get(item).?;
+            result += item * count;
+        }
+    }
+
+    return result;
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -54,25 +93,10 @@ pub fn main() !void {
         std.debug.print("Length of left and right columns is not equal!", .{});
         return;        
     }
-    
-    std.mem.sort(i32, left_column.items, {}, lessThan);
-    std.mem.sort(i32, right_column.items, {}, lessThan);
 
-    std.debug.print("Sorted left column ({d} items): ", .{left_column.items.len});
-    for (left_column.items) |num| {
-        std.debug.print("{d} ", .{num});
-    }
-    std.debug.print("\n\n", .{});
+    const first_result = try partOne(left_column, right_column);
+    std.debug.print("Part 1: difference between columns is {}\n", .{first_result});
 
-    std.debug.print("Sorted right column ({d} items): ", .{right_column.items.len});
-    for (right_column.items) |num| {
-        std.debug.print("{d} ", .{num});
-    }
-    std.debug.print("\n\n", .{});
-
-    var result: u32 = 0;
-    for (left_column.items, right_column.items) |item1, item2| {
-        result += @intCast(@abs(item1 - item2));
-    }
-    std.debug.print("Result: {d}\n\n", .{result});
+    const second_result = try partTwo(left_column,right_column);
+    std.debug.print("Part 2: similarity score {}\n", .{second_result});
 }
